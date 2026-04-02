0b6918dafd5f3532c40871ddd4488d0cd6669ae6
Roman <lackroy5111@gmail.com>
2026-04-02 11:03:49 +0000

    backoff для запросов

--- api/src_api/core/db/cache.py
+++ api/src_api/core/db/cache.py
@@ -1,3 +1,4 @@
+from src_api.utils.backoff import Backoff
 from src_api.core.db.exceptions import SaveRedisCacheError
 from src_api.features.shared.types import DataclassType
 from abc import ABC, abstractmethod
@@ -6,6 +7,7 @@ from dataclasses import asdict, is_dataclass
 from typing import Any, TypeVar
 
 import redis.asyncio as aioredis
+from redis.exceptions import ConnectionError, TimeoutError
 
 from src_api.core.config.settings import settings
 
@@ -17,43 +19,48 @@ T = TypeVar("T")
 
 class CacheClientInterface(ABC):
     @abstractmethod
-    async def set_cache(self, key: str, dto_obj: T) -> None:
-        ...
+    async def set_cache(self, key: str, dto_obj: T) -> None: ...
 
     @abstractmethod
-    async def get_cache(self, key: str, result_dto_class: type[T]) -> T | None:
-        ...
+    async def get_cache(self, key: str, result_dto_class: type[T]) -> T | None: ...
 
     @abstractmethod
-    async def delete_cache(self, key: str) -> int:
-        ...
+    async def delete_cache(self, key: str) -> int: ...
 
     @abstractmethod
     def build_cache_key(self, prefix: str, *key_args: Any) -> str:  # noqa: ANN401
         ...
 
 
+RETRY_EXCEPTIONS = (
+    ConnectionError,
+    TimeoutError,
+)
+
+
 class RedisCacheClient(CacheClientInterface):
     OBJ_NOT_DATACLASS_MSG = "Object must be a dataclass"
-    
+
     def __init__(self, client: aioredis.Redis, ttl: int) -> None:
         self.client = client
         self.ttl = ttl
 
+    @Backoff(RETRY_EXCEPTIONS)
     async def set_cache(self, key: str, dto_obj: DataclassType) -> None:
         if not is_dataclass(dto_obj):
             raise ValueError(self.OBJ_NOT_DATACLASS_MSG)
-        
+
         data = asdict(dto_obj)
         str_data = json.dumps(data)
         is_saved = await self.client.set(key, str_data, ex=self.ttl)
         if not is_saved:
             raise SaveRedisCacheError("Failed to save movie to cache")
 
+    @Backoff(RETRY_EXCEPTIONS)
     async def get_cache(self, key: str, result_dto_class: type[T]) -> T | None:
         if not is_dataclass(result_dto_class):
             raise ValueError(self.OBJ_NOT_DATACLASS_MSG)
-        
+
         raw_data = await self.client.get(key)
         if not raw_data:
             return None
@@ -62,9 +69,10 @@ class RedisCacheClient(CacheClientInterface):
         data = json.loads(json_str)
         return result_dto_class(**data)
 
+    @Backoff(RETRY_EXCEPTIONS)
     async def delete_cache(self, key: str) -> int:
         return await self.client.delete(key)
-    
+
     def build_cache_key(self, prefix: str, *key_args: Any) -> str:  # noqa: ANN401
         key = ":".join(map(str, key_args))
         return f"{prefix}:{key}"
--- api/src_api/core/db/elastic_db.py
+++ api/src_api/core/db/elastic_db.py
@@ -1,8 +1,35 @@
+from typing import Any
+from src_api.utils.backoff import Backoff
 from elasticsearch import AsyncElasticsearch
+from elastic_transport import ObjectApiResponse
 
 from src_api.core.config.settings import settings
 
-client = AsyncElasticsearch(
+from elastic_transport import (
+    ConnectionError,
+    ConnectionTimeout,
+    TlsError,
+)
+
+
+RETRY_EXCEPTIONS = (
+    ConnectionError,
+    ConnectionTimeout,
+    TlsError,
+)
+
+
+class BackoffAsyncElasticsearch(AsyncElasticsearch):
+    @Backoff(RETRY_EXCEPTIONS)
+    async def get(self, *args: Any, **kwargs: Any) -> ObjectApiResponse:  # noqa: ANN401
+        return await super().get(*args, **kwargs)
+
+    @Backoff(RETRY_EXCEPTIONS)
+    async def search(self, *args: Any, **kwargs: Any) -> ObjectApiResponse:  # noqa: ANN401
+        return await super().search(*args, **kwargs)
+
+
+client = BackoffAsyncElasticsearch(
     hosts=[settings.elastic_base_url],
 )
 
