eabe9e8ccef230718310ca21b658ed4e26a08e58
Roman <lackroy5111@gmail.com>
2026-05-05 10:47:46 +0000

    Локализация приложения пользователей

--- admin_panel/users/models.py
+++ admin_panel/users/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 from django.contrib.auth.base_user import BaseUserManager
 
+from django.utils.translation import gettext_lazy as _
+
 
 class MyUserManager(BaseUserManager):
     def create_user(self, email: str, password: str | None = None) -> User:
@@ -26,34 +28,53 @@ class MyUserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
+    password = None
+    
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False,
     )
     email = models.EmailField(
-        verbose_name="email address",
+        verbose_name=_("email address"),
         max_length=255,
         unique=True,
     )
-    is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255, null=True, blank=True)
+    is_active = models.BooleanField(
+        verbose_name=_("is active"),
+        default=True,
+    )
+    is_superuser = models.BooleanField(
+        verbose_name=_("is superuser"),
+        default=False,
+    )
+    is_staff = models.BooleanField(
+        verbose_name=_("is staff"),
+        default=False,
+    )
+    first_name = models.CharField(
+        verbose_name=_("first name"),
+        max_length=255,
+    )
+    last_name = models.CharField(
+        verbose_name=_("last name"),
+        max_length=255,
+        null=True,
+        blank=True,
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
     )
     updated_at = models.DateTimeField(
         auto_now=True,
     )
-
+    
     USERNAME_FIELD = "email"
 
     objects = MyUserManager()
 
     def __str__(self) -> str:
-        return f"{self.email} {self.id}"
+        return f"{self.email} {self.first_name} {self.last_name}"
 
     def has_perm(self, perm: str, obj: User | None = None) -> bool:
         return True
@@ -63,5 +84,5 @@ class User(AbstractBaseUser):
 
     class Meta:
         db_table = 'users"."user'
-        verbose_name = "User"
-        verbose_name_plural = "Users"
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
